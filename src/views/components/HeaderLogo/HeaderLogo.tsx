35caf834fcf34b7d04e17f7656c7141163594571
ShkarupaDenys <shkarupadenys@gmail.com>
2023-06-21 19:04:18 +0300

    Favicon was added.

--- src/views/components/HeaderLogo/HeaderLogo.tsx
+++ src/views/components/HeaderLogo/HeaderLogo.tsx
@@ -2,7 +2,7 @@ import { Logo } from "views/icons/Logo";
 import './HeaderLogo.scss'
 
 export const HeaderLogo = () => (
-  <a href="/" className="HeaderLogo">
+  <a href="/#" className="HeaderLogo">
     <Logo />
     <span className="HeaderLogo__name">TESTTASK</span>
   </a>
